27e733262b1bac45405ee3195ba0f54176a90151
Hui Sheng <huisheng@ximnet.com.my>
2022-04-06 17:40:27 +0800

    Add UseSpaStaticFiles

--- RealTimeTodo.Web/Program.cs
+++ RealTimeTodo.Web/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSpaStaticFiles(configure =>
+           {
+             configure.RootPath = "wwwroot";
+           });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+//app.MapGet("/", () => "Hello World!");
+
+app.UseSpaStaticFiles();
+
+app.UseSpa(config =>
+{
+  config.UseProxyToSpaDevelopmentServer("http://localhost:8080");
+});
 
 app.Run();
